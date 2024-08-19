5161b2332515824c5c9ce55ba4c2acadeeb23e73
Michel Alves <ewave.malves@tcp.com.br>
2024-08-19 15:32:10 -0300

    create sidebar and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarService } from './_services/sideBar/sidebar.service';
+
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+  PoPageAction,
+  PoBreadcrumb
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+
+  CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule
+  ],
+  providers: [SidebarService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proof-of-delivery';
+
+  menuItemSelected: string = '';
+
+  public readonly breadcrumb: PoBreadcrumb = {
+    items: [{ label: 'Home', link: '/' }, { label: 'Dashboard' }]
+   };
+
+   public readonly actions: Array<PoPageAction> = [
+    
+  ];
+
+  menus: Array<PoMenuItem> = [
+ 
+    { label: 'Dashboard',  action: this.printMenuAction.bind(this), icon: 'po-icon po-icon-chart-columns', link: 'dashboard' , shortLabel: 'dashboard'},
+    {
+      label: 'Cotações',
+      icon: 'po-icon po-icon-handshake',
+      shortLabel: 'Cotações',
+      subItems: [
+        { label: 'Criar', link: 'cotacoes/criar' },
+        { label: 'Listar', link: 'cotacoes/listar' }
+      ]
+    },
+    {
+      label: 'Pedidos',
+      icon: 'po-icon po-icon-finance-secure',
+      shortLabel: 'Pedidos',
+      subItems: [
+        { label: 'Listar', link: 'pedidos/listar' },
+        { label: 'FollowUp', link: 'pedidos/followup' }
+      ]
+    },
+   
+  ];
+  constructor(public SidebarService: SidebarService) {}
+  printMenuAction(menu: PoMenuItem) {
+    this.menuItemSelected = menu.label;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    // {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // },
+    // {
+    //     path: 'pedidos/listar',
+    //     component: PedidosComponent
+    // },
+    // {
+    //     path: 'pedidos/followup',
+    //     component: PedidosComponent
+    // },
+    // {
+    //     path: 'cotacoes/criar',
+    //     component: PedidosComponent
+    // },
+    // {
+    //     path: 'cotacoes/listar',
+    //     component: PedidosComponent
+    // }
+
+];
