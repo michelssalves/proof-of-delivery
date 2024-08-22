a9768a0a35b7b263bc3f475819ca53017fc9cc3f
Michel Alves <ewave.malves@tcp.com.br>
2024-08-22 18:50:39 -0300

    criado tabela e modal para medicoes

--- src/app/_services/medicoes/medicoes.service.ts
+++ src/app/_services/medicoes/medicoes.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoesService {
 
-  constructor() { }
+  private apiUrl = 'http://vhwin1065:9023/rest/zWSMedicoes/get_all/';
+  private apiUrlgetMaxPurchases = 'http://vhwin1065:9023/rest/zWSPedidos/get_max_purchases';
+  private apiUrlgetMaxPurchasesTab = 'http://vhwin1065:9023/rest/zWSPedidos/get_max_purchases_tab';
+  private apiUrlgetMaxPurchasesCol = 'http://vhwin1065:9023/rest/zWSPedidos/get_max_purchases_col/';
+  private apiUrlgetMaxPurchasesLine = 'http://vhwin1065:9023/rest/zWSPedidos/get_max_purchases_line/';
+  
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  postData(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, data);
+  }
+
 }
+
+
+
--- src/app/medicoes/medicoes.component.ts
+++ src/app/medicoes/medicoes.component.ts
@@ -1,12 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MedicoesService } from '../_services/medicoes/medicoes.service';
+import { 
+  PoContainerModule, 
+  PoWidgetModule, 
+  PoPageModule, 
+  PoModalModule, 
+  PoFieldModule, 
+  PoDynamicModule,
+  PoTableModule, 
+  PoModalAction, 
+  PoModalComponent, 
+  PoButtonModule 
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-medicoes',
   standalone: true,
-  imports: [],
+  imports: [
+    PoTableModule, 
+    PoContainerModule, 
+    PoPageModule,
+    PoWidgetModule,
+    PoModalModule,
+    PoFieldModule,
+    PoDynamicModule,
+    CommonModule,
+    PoButtonModule,
+    FormsModule
+  ],
   templateUrl: './medicoes.component.html',
-  styleUrl: './medicoes.component.css'
+  styleUrls: ['./medicoes.component.css']
 })
 export class MedicoesComponent {
+  @ViewChild(PoModalComponent, { static: true }) poModal!: PoModalComponent;
+  @ViewChild('optionsForm', { static: true }) form!: NgForm;
 
+  medicoes: any = [];
+  selectedItem: any = {};
+
+  close: PoModalAction = {
+    action: () => this.closeModal(),
+    label: 'Close',
+    danger: true
+  };
+
+  confirm: PoModalAction = {
+    action: () => this.processOrder(),
+    label: 'Confirm'
+  };
+
+  constructor(private dataService: MedicoesService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe(
+      response => {
+        this.medicoes = response.objects; // Supondo que a resposta seja { objects: [...] }
+        console.log(this.medicoes);
+      },
+      error => {
+        console.error('Erro ao obter dados:', error);
+      }
+    );
+  }
+
+  columns = [
+    { property: 'medicao', label: 'Medição' },
+    { property: 'contrato', label: 'Nº Contrato' },
+    { property: 'revisao', label: 'Nº Revisao' },
+    { property: 'competencia', label: 'Competencia' },
+    { property: 'descricao', label: 'Descricao' },
+    { property: 'valor', label: 'Valor' }
+  ];
+  event: string = '';
+  changeEvent(event: string) {
+    this.event = event;
+  }
+  openModal(item: any) {
+
+    
+    this.selectedItem = item;
+    console.log(this.selectedItem)
+    this.poModal.open();
+  }
+
+  closeModal() {
+    this.form.reset();
+    this.poModal.close();
+  }
+
+  processOrder() {
+    if (this.form.invalid) {
+      // Exibir mensagem de erro
+      console.error('Escolha os itens para confirmar o pedido.');
+    } else {
+      this.confirm.loading = true;
+
+      setTimeout(() => {
+        this.confirm.loading = false;
+        this.closeModal();
+      }, 700);
+    }
+  }
+
+  restore() {
+    this.form.reset();
+  }
 }
